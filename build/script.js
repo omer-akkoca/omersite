f505b96cd92c32b6e690dde1dbaec4b3d4a66447
omer-akkoca <omerakkoca_1042@mail.com.tr>
2021-03-09 23:28:23 +0300

    changing homepage to have custom domain

---
+++ build/script.js
@@ -0,0 +1,36 @@
+// Menunun responsive yapımı
+const menu = document.querySelector(".menu");
+const responsiveMenu = document.querySelector(".responsiveMenu");
+const menus = document.querySelectorAll(".navbar ul a");
+
+menu.addEventListener("click" , () => {
+    responsiveMenu.classList.toggle("active");
+});
+
+menus.forEach(e =>{
+    e.addEventListener("click" , () =>{
+        responsiveMenu.classList.remove("active");
+    });
+});
+
+
+// Sayfalar asagı kaydıgında navbarın css degısmesı alması
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll" , () =>{
+    if(window.scrollY > 60){
+        navbar.classList.add("active");
+    }else{
+        navbar.classList.remove("active");
+    }
+})
+
+
+// menuye tıkladıgımda scroolun sıfırlanması
+const li_es = document.querySelectorAll(".option");
+
+li_es.forEach(li =>{
+    li.addEventListener("click" , () =>{
+        window.scrollTo(0, 0);
+    });
+});
